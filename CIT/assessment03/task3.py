4f74ec6ca48fc87cc8d3905aa2ec6d666f16c1ca
b-mccormack <blake.mccormack91@outlook.com>
2020-08-31 15:46:05 +1000

    Work on menu

--- CIT/assessment03/task3.py
+++ CIT/assessment03/task3.py
@@ -12,20 +12,41 @@ def main():
     print("\nWelcome to the WIDGET inventory management system\n")
     menuText = "\nPlease select from the following menu options:\n\n\
         1 = Enter staff details\n\
-        2 = "
+        2 = See staff pay rates\n\
+        3 = See a list of required IT hardware\n\
+        4 = See the costing for IT hardware\n\
+        5 = Exit\n\n\
+        Please enter your selection: "
     menuSelection = str(input(menuText))
+    if menuSelection == '1':
+        addStaffDetails()
+    elif menuSelection == '2':
+        staffPayRates()
+    elif menuSelection == '3':
+        gadgetList()
+    elif menuSelection == '4':
+        gadgetCosting()
+    elif menuSelection == '5':
+        exit()
+    else:
+        print("\nError: You did not enter a valid menu selection. Please enter a number between 1 and 4")
+        main()
 
 def addStaffDetails():
-    print()
+    print('This is addStaffDetails')
 
 def staffPayRates():
-    print()
+    print('This is staffPayRates')
 
 def gadgetList():
-    print()
+    print('This is gadgetList')
 
 def gadgetCosting():
-    print()
+    print('This is gadgetCosting')
 
 
 main()
+
+
+
+print(staffDetails['Some Person'][1])
