f9d103347731c7d267cec465a42959670e42bd33
b-mccormack <blake.mccormack91@outlook.com>
2020-08-31 15:14:11 +1000

    Initial work on task 3

--- CIT/assessment03/task3.py
+++ CIT/assessment03/task3.py
@@ -1,4 +1,31 @@
 # Author : Blake McCormack
-# Date: 24/08/2020
+# Date: 31/08/2020
 # Version 1.0
 #
+# This program is designed to be a simple inventory management system for a small accounting firm
+
+# Define the main function which will be called when the application starts
+
+staffDetails = {'Some Person' : ['0400111222','Accountant','95000']}
+
+def main():
+    print("\nWelcome to the WIDGET inventory management system\n")
+    menuText = "\nPlease select from the following menu options:\n\n\
+        1 = Enter staff details\n\
+        2 = "
+    menuSelection = str(input(menuText))
+
+def addStaffDetails():
+    print()
+
+def staffPayRates():
+    print()
+
+def gadgetList():
+    print()
+
+def gadgetCosting():
+    print()
+
+
+main()
