65779c7b79dc10d9c2aa5be836f059ba87585100
b-mccormack <blake.mccormack91@outlook.com>
2020-08-31 15:49:12 +1000

    Moved comment into the correct place

--- CIT/assessment03/task3.py
+++ CIT/assessment03/task3.py
@@ -4,10 +4,9 @@
 #
 # This program is designed to be a simple inventory management system for a small accounting firm
 
-# Define the main function which will be called when the application starts
-
 staffDetails = {'Some Person' : ['0400111222','Accountant','95000']}
 
+# Define the main function which will be called when the application starts
 def main():
     print("\nWelcome to the WIDGET inventory management system\n")
     menuText = "\nPlease select from the following menu options:\n\n\
