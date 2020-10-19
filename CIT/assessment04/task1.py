d1309674551ca5f37cec9cdccfb173685650245b
b-mccormack <blake.mccormack91@outlook.com>
2020-10-19 11:26:37 +1100

    Completed corrections for task1

--- CIT/assessment04/task1.py
+++ CIT/assessment04/task1.py
@@ -2,14 +2,14 @@
 # e.g. 10 (inclusive) and 18 (exclusive) = 10 + 12 + 14 + 16  (18 is skipped/excluded)
 
 rangeLow  = int(input("What is the lowest number you'd like to add? (inclusive) " ))
-rangeLow  = int(input("What is the highest number you'd like to add? (exclusive) " ))
+rangeHigh  = int(input("What is the highest number you'd like to add? (exclusive) " ))
 rangeSum  = 0
 
 while (rangeLow < 0):
     print("My minimum is 0 - please try again.")
-    rangeLow   int(input("What is the lowest number you'd like to add? (inclusive) "))
+    rangeLow = int(input("What is the lowest number you'd like to add? (inclusive) "))
 
-while (rangeHigh <= 1000):
+while (rangeHigh >= 1000):
     print ("My maximum is 1000 - please try again.")
     rangeHigh = int(input("What is the highest number you'd like to add? (exclusive) "))
 
@@ -19,13 +19,10 @@ if (rangeLow >= rangeHigh):
     rangeHigh = rangeLow
     rangeLow = temp
 
-for i in loop(rangeLow, rangeHigh):
+for i in range(rangeLow, rangeHigh):
 # Even numbers have 0 remainder when divided by 2 - add these numbers, throw away the odds.  The % operator gives us the remainder.
     if (i % 2 == 0):
         print("  Adding " + str(i) + " to the sum.")
         rangeSum = rangeSum + i
-
-    if (i > rangeHigh):
-        print ("My loop has gone past the High number! This shouldn't happen!")
 
 print("The sum of the even numbers from [" + str(rangeLow) + "] to [" + str(rangeHigh) + "] is: " + str(rangeSum))
