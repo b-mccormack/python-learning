f0c8e3e09a616c741f48c3db8d5a6d39184ace19
b-mccormack <blake.mccormack91@outlook.com>
2020-10-19 19:16:34 +1100

    Completed task 3

--- CIT/assessment04/task3.py
+++ CIT/assessment04/task3.py
@@ -7,11 +7,17 @@
 # Import the regex python module
 import re
 
+# Ask the user for an email address that they would like to check for validity
 userInput = str(input('\nPlease enter an email address you would like to validate:\n'))
 
+# Assign regular expression to a variable that can be used in a function.
 emailRegex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
 
-if(re.search(emailRegex,userInput)):
-    print("Valid Email")
-else:
-    print("Invalid Email")
+# Define the function that will accept a regular expression and a string to test as inputs and print whether the email address is valid or not
+def check_validity(regex: str,email: str):
+    if(re.search(regex,email)):
+        print("That is a valid email address")
+    else:
+        print("Sorry, that is not a valid email address")
+
+check_validity(emailRegex,userInput)
